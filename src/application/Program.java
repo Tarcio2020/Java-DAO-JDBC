23f3c2d937110240c273297838333a4b1c00bf51
Tarcio2020 <tarcioapps7@gmail.com>
2025-01-24 10:41:01 -0300

    Department seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+
+import java.sql.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(2, "rh");
-		
+	
+	
+		Seller obj2 = new Seller(1, "Tarcio", "tarcio@example.com", Date.valueOf("2024-01-01"), 3000.0);
+
 		System.out.println(obj);
+		System.out.println(obj2);
+
 	}
 
 }
