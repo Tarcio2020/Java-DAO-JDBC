304fc4f2652e7cb005f9c1c69fec76ff4d7d0f74
Tarcio2020 <tarcioapps7@gmail.com>
2025-01-24 10:28:32 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj = new Department(2, "rh");
+		
+		System.out.println(obj);
+	}
+
+}
