70f731f029a55f940a38cecdbbc4d9f276faf540
Tarcio2020 <tarcioapps7@gmail.com>
2025-01-25 11:04:20 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
 import java.sql.Date;
+import java.util.List;
 
 import model.DAO.SellerDao;
 import model.DAO.impl.DaoFactory;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,8 +13,15 @@ public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
         
+		System.out.println("=== TEST 1: findById =======");
         Seller seller = sellerDao.findById(3); 
-
         System.out.println(seller);
+        
+		System.out.println("\n=== TEST 2: findBy Department=======");
+        Department department = new Department(1, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list) {
+        	System.out.println(obj);
+        }
     }
 }
--- src/model/DAO/impl/SellerDAOjdbc.java
+++ src/model/DAO/impl/SellerDAOjdbc.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DbException;
 import db.DbJava;
@@ -14,68 +17,120 @@ import model.entities.Seller;
 
 public class SellerDaoJdbc implements SellerDao {
 
-    private Connection conn;
+	private Connection conn;
 
-    public SellerDaoJdbc(Connection conn) {
-        this.conn = conn;
-    }
+	public SellerDaoJdbc(Connection conn) {
+		this.conn = conn;
+	}
 
-    @Override
-    public void insert(Seller obj) {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+	}
 
-    @Override
-    public void update(Seller obj) {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+	}
 
-    @Override
-    public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+	}
 
-    @Override
-    public Seller findById(Integer id) {
+	@Override
+	public Seller findById(Integer id) {
 
-        PreparedStatement st = null;
-        ResultSet rs = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
 
-        try {
-            st = conn.prepareStatement(
-                "SELECT seller.*, department.Name as DepName " + // Adicionado espaço antes de "FROM"
-                "FROM seller INNER JOIN department " +
-                "ON seller.DepartmentId = department.Id " +
-                "WHERE seller.Id = ?"
-            );
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName " + 
+							"FROM seller INNER JOIN department " +
+							"ON seller.DepartmentId = department.Id " +
+							"WHERE seller.Id = ?"
+					);
 
-            st.setInt(1, id);
-            rs = st.executeQuery();
-            if (rs.next()) {
-                Department dp = new Department();
-                dp.setId(rs.getInt("DepartmentId"));
-                dp.setName(rs.getString("DepName"));
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dp);
-                return obj; // Retorne o objeto Seller
-            }
-            return null;
-        } catch (SQLException e) {
-            throw new DbException(e.getMessage());
-        } finally {
-            DbJava.closeStatement(st);
-            DbJava.closeResultSet(rs);
-        }
-    }
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Department dp = instatiateDepartment(rs);
 
-    @Override
-    public List<Seller> findAll() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+				Seller obj = instatiateSeller(rs, dp);
+				return obj; 
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DbJava.closeStatement(st);
+			DbJava.closeResultSet(rs);
+		}
+	}
+
+	private Seller instatiateSeller(ResultSet rs, Department dp) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dp);
+		return obj;
+	}
+
+	private Department instatiateDepartment(ResultSet rs) throws SQLException {
+		Department dp = new Department();
+		dp.setId(rs.getInt("DepartmentId"));
+		dp.setName(rs.getString("DepName"));		
+		return dp;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName " +
+					"FROM seller INNER JOIN department " +
+					"ON seller.DepartmentId = department.Id " +
+					"WHERE DepartmentId = ? " +
+					"ORDER BY Name"
+					);
+
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) {
+				Department dp = map.get(rs.getInt("DepartmentId"));
+				
+				if (dp == null) {
+					dp = instatiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dp);
+				}
+				
+				Seller obj = instatiateSeller(rs, dp);
+				list.add(obj);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DbJava.closeStatement(st);
+			DbJava.closeResultSet(rs);
+		}
+	}
 }
